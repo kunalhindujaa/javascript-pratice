d83c0769d464a7bdebd24333f8dfab314fe4133a
Kunal Hinduja <48852726+kunalhindujaa@users.noreply.github.com>
2020-10-06 20:43:00 +0530

    Added way to add multiple properties to Prototype

--- js prototype/prototype.js
+++ js prototype/prototype.js
@@ -31,3 +31,25 @@ console.log(admin);
 //1. We can add properties and new functions to the prototype which wasn't possible when we were using Constructors.
 //2. We should keep functions seprate, e.g. register() and not in Prototype object creation template because
 //using the prototype makes for faster object creation, since that function does not have to be re-created each time a new object is created.
+
+/*
+Also instead of adding multiple properties for a single prototype individually, This way we are changing a prototype to a new object and adding properties all at once
+
+Bird.prototype = {
+numLegs: 2,
+eat: funcion(){
+console.log("I like to eat bird food");
+}
+};
+*/
+
+/*
+Own Properties and Prototype Properties
+
+function Bird(name) 
+{
+  this.name = name; // Here name is 'Own Property'
+}
+
+Bird.prototype.numLegs = 2; // Here numLegs is 'Prototype Property'
+*/
